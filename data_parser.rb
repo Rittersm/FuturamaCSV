8148cbc236a8ce9ed8a8905a1f5e7adb09e03735
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-06-27 20:53:31 -0400

    ran rubocop

--- data_parser.rb
+++ data_parser.rb
@@ -1,13 +1,14 @@
 require 'erb'
 require 'csv'
 
+# Class objects are rows of the csv file brought in.
 class Delivery
   @@total_revenue = 0
   @@all_pilots = []
   @@planets = []
   @@pilot_money = {}
   @@planet_money = {}
-  attr_accessor :destination, :shipment, :crates, :money, :pilot, :bonus,
+  attr_accessor :destination, :shipment, :crates, :money, :pilot, :bonus
 
   def self.total_revenue
     @@total_revenue
@@ -29,10 +30,8 @@ class Delivery
     @@planet_money
   end
 
-
   def pilot_money_writer
-    # unless defined?(@@pilot_data)
-      @@pilot_money[pilot] = @@pilot_money[pilot].to_i + money
+    @@pilot_money[pilot] = @@pilot_money[pilot].to_i + money
   end
 
   def planet_money_writer
@@ -45,73 +44,50 @@ class Delivery
     @crates = crates
     @money = money
     @pilot = determine_pilot[destination.to_sym]
-    @pilot1 = @pilot + "1"
     @bonus = money / 10.0
     @@total_revenue += money
-      unless @@all_pilots.include?(pilot)
-        @@all_pilots << pilot
-      end
-      unless @@planets.include?(destination)
-        @@planets << destination
-      end
+    @@all_pilots << pilot unless @@all_pilots.include?(pilot)
+    @@planets << destination unless @@planets.include?(destination)
     pilot_money_writer
     planet_money_writer
   end
 
   def determine_pilot
-    pilots = { Earth: "Fry", Uranus: "Bender", Mars: "Amy"}
-    pilots.default = "Leela"
+    pilots = { Earth: 'Fry', Uranus: 'Bender', Mars: 'Amy' }
+    pilots.default = 'Leela'
     pilots
   end
 
-  def Delivery.pilot_deliveries(pilot)
-    delivery_objects.select{|delivery| delivery.pilot == pilot}
+  def self.pilot_deliveries(pilot)
+    delivery_objects.select { |delivery| delivery.pilot == pilot }
   end
 end
 
 delivery_objects = []
 
-CSV.foreach("planet_express_logs.csv", headers: true) do |row|
-  delivery_objects << Delivery.new(row["Destination"], row["Shipment"], row["Crates"].to_s.to_i, row["Money"].to_s.to_i)
+CSV.foreach('planet_express_logs.csv', headers: true) do |row|
+  delivery_objects << Delivery.new(
+    row['Destination'],
+    row['Shipment'],
+    row['Crates'].to_s.to_i,
+    row['Money'].to_s.to_i
+  )
 end
 
-pilots = delivery_objects.collect{|delivery| delivery.pilot}.uniq
-
-planets = delivery_objects.collect{|planet| planet.destination}.uniq
+pilots = delivery_objects.collect(&:pilot).uniq
 
 pilot_data = []
 
 pilot_data << pilots.map do |pilot|
   {
     pilot: pilot,
-    deliveries: delivery_objects.select{|delivery| delivery.pilot == pilot}.length.to_i,
-    bonus: delivery_objects.select{|delivery| delivery.pilot == pilot}.collect{|delivery| delivery.bonus}.inject(:+).to_i
+    deliveries: delivery_objects.each do |delivery|
+      delivery.pilot == pilot
+    end.length.to_i,
+    bonus: delivery_objects.select { |delivery| delivery.pilot == pilot }.collect(&:bonus).inject(:+).to_i
   }
 end
 
-revenue = []
-
-revenue << pilots.map do |pilot|
-  {
-    pilot: pilot,
-    revenue: delivery_objects.select{|delivery| delivery.pilot == pilot}.collect{|delivery| delivery.money}.inject(:+)
-  }
-end
-
-revenue_by_planet = []
-
-revenue_by_planet << planets.map do |planet|
-  {
-    planet: planet,
-    revenue: delivery_objects.select{|delivery| delivery.destination == planet}.collect{|delivery| delivery.money}.inject(:+)
-  }
-end
-
-new_file = File.open("./report.html", "w+")
-new_file << ERB.new(File.read("./report.html.erb")).result(binding)
+new_file = File.open('./report.html', 'w+')
+new_file << ERB.new(File.read('./report.html.erb')).result(binding)
 new_file.close
-
-puts delivery_objects.inspect
-
-puts Delivery.pilot_money.to_a.inspect
-puts Delivery.planet_money.inspect
